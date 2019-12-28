3bf03e1737918d5e931a732587dda40bb33818f7
zhangpengyf <zhangpengyf@gmail.com>
2019-12-28 17:29:47 +0800

    save

--- node_modules/videojs-contrib-hls.js/examples/commonjs-app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var videojs = require('video.js');
-require('videojs-contrib-media-sources'); // increase browser support with MSE polyfill
-require('videojs-contrib-hlsjs'); // auto attaches hlsjs handler
-
-
-var player = videojs('#player');
